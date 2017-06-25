b74b9b09e5309889c98dfed8199b26d15b71d8f5
Maximilian <max.ragmo@outlook.com>
2017-06-25 17:41:01 +0200

    pixel experiment

---
+++ sketches/framing/sketch.js
@@ -0,0 +1,42 @@
+var img;
+
+function preload() {
+  img = loadImage("obama.jpg");
+}
+
+function setup(){
+  //pixelDensity(1.0);
+  canvas = createCanvas(windowWidth, windowHeight);
+  updateImage();
+}
+
+function draw() {
+  image(img, 0, 0);
+}
+
+
+function updateImage() {
+  img.loadPixels();
+
+  for (var i = 0; i < img.pixels.length; i+=4) {
+    red1 = img.pixels[i];
+    green1 = img.pixels[i + 1];
+    blue1 = img.pixels[i + 2];
+
+    if (within(red1, 0, 100) && within(green1, 100, 255) && within(blue1, 0, 100)) {
+      img.pixels[i] = 255;
+      img.pixels[i + 1] = 255;
+      img.pixels[i + 2] = 255;
+    }
+  }
+
+  img.updatePixels();
+
+}
+
+function within(v, min, max) {
+  if(v >= min && v <= max) {
+    return true;
+  }
+  return false;
+}
