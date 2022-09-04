3f368a6573e6dacbbf33469f23431ba9e960c75e
Maximilian <max.ragmo@outlook.com>
2022-09-04 18:53:11 +0200

    refactor

--- sketches/coderain/sketch.js
+++ sketches/coderain/sketch.js
@@ -1,105 +1,94 @@
-var useFont;
-var symbolSize = 26;
-var streams = [];
+const STREAMS = [];
+const FONT_SIZE = 26;
+const COLORS = {};
 
+let font;
 function preload() {
-  useFont = loadFont('RobotoMono-Regular.ttf');
+  font = loadFont("RobotoMono-Regular.ttf");
 }
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-
   background(0);
+  textFont(font);
+  textSize(FONT_SIZE);
 
-  var x = 0;
-  for (var i = 0; i <= width / symbolSize; i++) {
+  COLORS.green = color(0, 255, 50);
+  COLORS.glow = color(219, 255, 219);
 
-    var stream = new Stream();
-    stream.generateSymbols(x, random(-1000, 0));
-    streams.push(stream);
-    x += symbolSize;
+  const columns = width / FONT_SIZE;
+  for (let i = 0; i <= columns; i++) {
+    const startOffset = random(-1000, 0);
+    const stream = new Stream(i * FONT_SIZE, startOffset);
+    STREAMS.push(stream);
   }
-
-  textFont(useFont);
-  textSize(symbolSize);
 }
 
 function draw() {
   background(0, 50);
 
-  streams.forEach(function(stream) {
-    stream.render();
-  });
+  STREAMS.forEach((stream) => stream.update());
+  STREAMS.forEach((stream) => stream.render());
 }
 
-function Symbol(x, y, first) {
-  this.x = x;
-  this.y = y;
-  this.value;
-  this.first = first;
+class Stream {
+  constructor(x, y) {
+    this.length = round(random(5, height / FONT_SIZE - 5));
+    this.speedInterval = round(random(2, 20));
+    this.glow = round(random(0, 3)) === 0;
+    this.symbols = [];
 
-  this.switchInterval = round(random(100, 200));
-
-  this.setToRandomSymbol = function() {
-    if (frameCount % this.switchInterval == 0) {
-        this.value = round(random(0,1));
-      //this.value = String.fromCharCode(0x30A0 + round(random(0,96)));
+    for (let i = 0; i <= this.length; i++) {
+      this.symbols.push(new Symbol(x, y));
+      y -= FONT_SIZE;
     }
   }
 
-  this.render = function() {
-    if (this.first) {
-      fill(180, 255, 180);
-    } else {
-      fill(0, 255, 50);
-    }
-
-    text(this.value, this.x, this.y);
-    this.rain();
-    this.setToRandomSymbol();
-  }
-
-  this.rain = function() {
-    this.y = (this.y >= height) ? 0 : this.y;
-  }
-
-}
-
-function Stream() {
-  this.symbols = [];
-  this.totalSymbols = round(random(5,30));
-  this.speedInterval = round(random(2, 20));
-
-  this.generateSymbols = function(x, y) {
-    var first = round(random(0, 4)) == 1;
-    for (var i = 0; i <= this.totalSymbols; i++) {
-      symbol = new Symbol(x, y, first);
-      symbol.setToRandomSymbol();
-      this.symbols.push(symbol);
-      y -= symbolSize;
-      first = false;
-    }
-
-  }
-
-  this.render = function() {
-    //move last element below first element
+  update() {
     if (frameCount % this.speedInterval == 0) {
-      var symbol = this.symbols.pop();
-
-
-      if (this.symbols[0].first) {
-        symbol.first = true;
-        this.symbols[0].first = false;
-      }
-
-      symbol.y = this.symbols[0].y + symbolSize;
+      const symbol = this.symbols.pop();
+      symbol.y = this.symbols[0].y + FONT_SIZE;
       this.symbols.unshift(symbol);
     }
 
-    this.symbols.forEach(function(symbol) {
+    this.symbols.forEach((symbol) => symbol.update());
+  }
+
+  render() {
+    this.symbols.forEach((symbol, index) => {
+      fill(
+        this.glow
+          ? lerpColor(COLORS.glow, COLORS.green, map(index, 0, 3, 0, 1))
+          : COLORS.green
+      );
       symbol.render();
     });
   }
+}
 
+class Symbol {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.value;
+    this.switchInterval = round(random(100, 200));
+
+    this.cycle();
+  }
+
+  cycle() {
+    if (frameCount % this.switchInterval == 0) {
+      this.value = round(random(0, 1));
+      //this.value = String.fromCharCode(0x30a0 + round(random(0, 96)));
+    }
+  }
+
+  update() {
+    this.y = this.y >= height ? 0 : this.y;
+    this.cycle();
+  }
+
+  render() {
+    text(this.value, this.x, this.y);
+  }
 }
