5db4728d74c2da17c6508d0a4761b9e13bc1c566
Maximilian <max.ragmo@outlook.com>
2022-10-01 00:14:41 +0200

    update pixlify

--- sketches/pixlify/sketch.js
+++ sketches/pixlify/sketch.js
@@ -1,85 +1,102 @@
-var img;
-var imgIcon;
-var pixelSize = 16;
-var maxPixels = 1000000;
-var minPixels = 250000;
+const settings = {
+  pixelSize: 16,
+  img: undefined,
+  fileName: "",
+};
 
-function preload() {
-  imgIcon = loadImage("imageicon.png");
-}
+let pane = new Tweakpane.Pane();
 
 function setup() {
-  var canvas_ = createCanvas(window.innerWidth, window.innerHeight);
-  canvas_.drop(receiveFile);
-
-  resetScreen('Drag image here');
-}
-
-function resetScreen(text1) {
-  background(230);
-  fill(0);
-  noStroke();
-  textSize(24);
-  textAlign(CENTER);
-  text(text1, width/2, height/2);
-  image(imgIcon, width/2 - imgIcon.width / 2, height/2 - imgIcon.height * 1.2);
-}
-
-function draw() {
-  // img.loadPixels();
-  //
-  // for (var i = 0; i < 100000; i++) {
-  //   var x = int(random(img.width));
-  //   var y = int(random(img.height));
-  //
-  //   var off = (x + y * img.width) * 4;
-  //   var pixs = [img.pixels[off + 0], img.pixels[off + 1],
-  //               img.pixels[off + 2], img.pixels[off + 3]];
-  //   fill(pixs);
-  //   var s = int(random(16, 24));
-  //   ellipse(x, y, s, s);
-  // }
-}
-
-function receiveFile(file) {
-  if (file.type === 'image') {
-    img = createImg(file.data, imageLoaded).hide();
-  }
-
-  resetScreen('Processing...');
-}
-
-function highlightOn() {
-  resetScreen('Drop');
-}
-
-function highlightOff() {
-  resetScreen('Drag image here');
-}
-
-function imageLoaded() {
-  if(img.width * img.height < minPixels) {
-    resetScreen('Image resolution to small');
+  if (typeof window.showOpenFilePicker !== "function") {
     return;
   }
 
-  var cnv = select('canvas');
-  var ctx = cnv.elt.getContext('2d');
-  ctx.drawImage(img.elt, 0, 0);
-  var rgba = ctx.getImageData(0, 0, img.width, img.height).data;
-  paintImage(rgba);
+  createCanvas(window.innerWidth, window.innerHeight);
+
+  pane = new Tweakpane.Pane();
+  pane
+    .addInput(settings, "pixelSize", {
+      min: 5,
+      max: 30,
+      step: 1,
+      label: "Pixelsize",
+    })
+    .on("change", (ev) => {
+      if (ev.last && settings.img) {
+        paintImage();
+      }
+    });
+  pane.addMonitor(settings, "fileName", {
+    label: "",
+    disabled: true,
+  });
+  pane
+    .addButton({
+      label: "Image",
+      title: "Select",
+    })
+    .on("click", () => selectImage());
 }
 
-function paintImage(pixels) {
-  background(230);
+async function selectImage() {
+  const filePickerOptions = {
+    types: [
+      {
+        description: "Images",
+        accept: {
+          "image/*": [".png", ".gif", ".jpeg", ".jpg"],
+        },
+      },
+    ],
+    excludeAcceptAllOption: true,
+    multiple: false,
+  };
 
-  for (var i = 0; i < pixels.length / pixelSize && i < maxPixels; i++) {
-    var x = int(random(img.width));
-    var y = int(random(img.height));
-    var off = (x + y * img.width) * 4;
-    var pixs = [pixels[off + 0], pixels[off + 1],
-                pixels[off + 2], pixels[off + 3]];
-    fill(pixs);
-    ellipse(x, y, pixelSize, pixelSize);
+  const [fileHandle] = await window.showOpenFilePicker(filePickerOptions);
+  const file = await fileHandle.getFile();
+  const fileReader = new FileReader();
+
+  settings.fileName = file.name;
+  pane.refresh();
+
+  fileReader.addEventListener("loadend", () => {
+    createImg(fileReader.result, (img) => {
+      settings.img = img;
+      paintImage();
+    }).hide();
+  });
+
+  fileReader.readAsDataURL(file);
+}
+
+function paintImage() {
+  pane.disabled = true;
+
+  const img = settings.img;
+  background(255);
+
+  const gp = createGraphics(width, height);
+  gp.image(img, 0, 0, width, height);
+  gp.loadPixels();
+
+  noStroke();
+  const density = pixelDensity();
+
+  const dots = gp.pixels.length / 4 / settings.pixelSize;
+  for (let i = 0; i < dots; i++) {
+    const x = int(random(gp.width));
+    const y = int(random(gp.height));
+    const index = 4 * (y * density * width * density + x * density);
+
+    const fillColor = [
+      gp.pixels[index + 0],
+      gp.pixels[index + 1],
+      gp.pixels[index + 2],
+      gp.pixels[index + 3],
+    ];
+    fill(fillColor);
+    ellipse(x, y, settings.pixelSize, settings.pixelSize);
   }
+
+  pane.disabled = false;
 }
