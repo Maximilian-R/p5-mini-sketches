2d1b0b62515591d4ed1b266207c28d0394806c54
Maximilian <max.ragmo@outlook.com>
2022-10-02 15:48:03 +0200

    convert to es6 and use tweakpane

--- sketches/winter/sketch.js
+++ sketches/winter/sketch.js
@@ -1,69 +1,78 @@
-var particles = [];
-var totalParticles = 200;
-var gravity;
-var wind;
-function mousePressed() {
-  wind = createVector(random(-2, 2), random(-0.4, 0.4));
-}
+const particles = [];
 
-function setup() {
-  canvas = createCanvas(window.innerWidth, window.innerHeight);
+let gravity;
+let wind;
 
-  // Give the canvas a gradient background. Drawing a gradient background each frame takes to much performance.
-  var can = document.getElementById("defaultCanvas0");
-  can.style.background = "-webkit-radial-gradient(#1b2735 0%,#090a0f 100%)";
-  can.style.background = "-o-radial-gradient(#1b2735,#090a0f 100%)";
-  can.style.background = "-moz-radial-gradient(#1b2735,#090a0f)";
-  can.style.background = "radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%";
-
-  // Spawn particles
-  for (var i = 0; i <= totalParticles; i++) {
-    var x = random(0, width);
-    var y = random(0, height);
-    var p = new particle(x, y);
-    particles.push(p);
-  }
-
-  gravity = createVector(0.0, 0.4);
-  wind = createVector(0, 0);
-
-}
-
-function draw() {
-  clear();
-
-  var yVel = map(height * 0.7, 0, height, -1, 1);
-  gravity.y = yVel;
-  var xVel = map(width * 0.5, 0, width, -1, 1);
-  gravity.x = xVel;
-
-
-  particles.forEach(function(p) {
-    p.update();
-  });
-
-}
+const settings = {
+  windX: 0,
+  windY: 0,
+};
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function particle(x, y) {
-  this.pos = createVector(x, y);
-  this.vel = createVector(0, 0);
-  this.fo = createVector(random(-0.04, 0.04), 0);
+function setup() {
+  canvas = createCanvas(window.innerWidth, window.innerHeight);
 
-  this.size = random(1, 6);
+  const canvasElement = document.getElementById("defaultCanvas0");
+  canvasElement.style.background =
+    "-webkit-radial-gradient(#1b2735 0%,#090a0f 100%)";
+  canvasElement.style.background = "-o-radial-gradient(#1b2735,#090a0f 100%)";
+  canvasElement.style.background = "-moz-radial-gradient(#1b2735,#090a0f)";
+  canvasElement.style.background =
+    "radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%";
 
-  this.update = function() {
+  for (let i = 0; i <= 200; i++) {
+    const particle = new Particle(random(0, width), random(0, height));
+    particles.push(particle);
+  }
 
-    var nos = random(-0.04, 0.04);
-    this.fo.add(nos);
-    this.fo.limit(2);
-    
-    // Calcualte position/movement
+  gravity = createVector(0.0, 0.4);
+  wind = createVector(0, 0);
+
+  setupGUI();
+}
+
+function draw() {
+  clear();
+
+  particles.forEach((particle) => {
+    particle.update();
+  });
+}
+
+function setupGUI() {
+  const pane = new Tweakpane.Pane();
+
+  pane
+    .addInput(settings, "windX", { label: "wind x", min: -5, max: 5 })
+    .on("change", (event) => {
+      wind.x = event.value;
+    });
+  pane
+    .addInput(settings, "windY", { label: "wind y", min: -5, max: 5 })
+    .on("change", (event) => {
+      wind.y = event.value;
+    });
+}
+
+class Particle {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.force = createVector(random(-0.04, 0.04), 0);
+
+    this.size = random(1, 6);
+  }
+
+  update() {
+    const noise = random(-0.04, 0.04);
+    this.force.add(noise);
+    this.force.limit(2);
+
     this.vel = p5.Vector.mult(gravity, this.size);
-    this.vel.add(this.fo);
+    this.vel.add(this.force);
     this.vel.add(wind);
     this.pos.add(this.vel);
 
@@ -71,24 +80,21 @@ function particle(x, y) {
     this.render();
   }
 
-  this.render = function() {
+  render() {
     stroke(255);
     strokeWeight(this.size);
     point(this.pos.x, this.pos.y);
   }
 
-  this.bounding = function() {
+  bounding() {
+    const margin = this.size;
 
-    var margin = 10;
-
-    // Check x
     if (this.pos.x >= width + margin) {
       this.pos.x = 0 - margin;
     } else if (this.pos.x <= 0 - margin) {
       this.pos.x = width + margin;
     }
 
-    // Check y
     if (this.pos.y >= height + margin) {
       this.pos.y = 0 - margin;
       this.pos.x = random(0, width);
